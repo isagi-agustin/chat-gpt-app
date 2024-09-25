4a5e0969cdefedec43ad405683e889369d31039e
isagi-agustin <isagiagustin@outlook.com>
2024-09-25 10:27:44 -0700

    require authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
+import { auth as getServerSession } from "@/auth";
+
 import Chat from '@/app/components/Chat';
 import { Separator } from '@/components/ui/separator';
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession();
+
   return (
     <main className="p-5">
       <h1 className="text-4xl font-bold">Welcome To GPT Chat</h1>
-      <Separator className="my-5" />
-      <Chat />
+      {!session?.user?.name && <div>You need to log in to use this chat.</div>}
+      {session?.user?.name && (
+        <>
+          <Separator className="my-5" />
+          <Chat />
+        </>
+      )}
     </main>
   );
 }
